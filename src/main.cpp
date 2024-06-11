25b13fe9ce307548ad960bb6ca567f027ac2fcfe
FrancisHerve <128503272+FrancisHerve@users.noreply.github.com>
2024-06-11 15:21:37 +0200

    Update main.cpp
    modification pour activer les barrière en fonction des sensor

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,12 @@
 ThreadLvgl threadLvgl(30);
 
 // Configuration des pins pour les servomoteurs et les capteurs
-PwmOut servoPin1(PA_8);
-PwmOut servoPin2(PB_15);
+PwmOut servoPin1(PA_8); //D10
+PwmOut servoPin2(PB_15);// D11
 DigitalIn sensor1(D2);
 DigitalIn sensor2(D3);
 DigitalIn sensor3(D4);
 DigitalIn sensor4(D5);
-
 
 // Constantes pour la période du servo et les positions
 const int SERVO_PERIOD_MS = 20; // Période du servo en ms
@@ -23,7 +22,8 @@ const float FT90M_pulseMax = 0.002;  // Impulsion pour pivoter le servo à 280º
 
 // Constantes pour les servomoteurs standards
 const float pulseMin = 0.00065;  // Impulsion pour pivoter le servo à 0º (650 μs)
-const float pulseMax = 0.00255;  // Impulsion pour pivoter le servo à 180º (2550 μs)
+const float pulseMax = 0.001275;  // Impulsion pour pivoter le servo à 90º (1275 μs)
+//const float pulseMax = 0.00255;  // Impulsion pour pivoter le servo à 180º (2550 μs)
 
 // Variables pour stocker les valeurs de mesure en degrés
 int angle1 = 0;  // Initialisé à la position correspondante
@@ -31,6 +31,12 @@ int angle2 = 0;  // Initialisé à la position correspondante
 
 lv_obj_t *servo1_label;
 lv_obj_t *servo2_label;
+lv_obj_t *sensor1_label;
+lv_obj_t *sensor2_label;
+lv_obj_t *sensor3_label;
+lv_obj_t *sensor4_label;
+lv_obj_t *car_detected_label;
+lv_obj_t *car_detected_label2;
 
 // Fonction pour mapper les valeurs (similaire à la fonction Arduino map)
 float map(float x, float in_min, float in_max, float out_min, float out_max) {
@@ -49,15 +55,14 @@ void setServoAngleFT90M(PwmOut &servoPin, int angle) {
     servoPin.pulsewidth(pulseWidth);
 }
 
-// Fonction pour récupérer la position du servomoteur
-int getServoAngle(PwmOut &servoPin) {
-    float pulseWidth = servoPin.read();
-    return (int)map(pulseWidth, pulseMin, pulseMax, 0, 180);
-}
-
 // Fonction pour mettre à jour le label de position du servomoteur
 void updateServoLabel(lv_obj_t *label, int angle) {
     lv_label_set_text_fmt(label, "Angle: %d deg", angle);
+}
+
+// Fonction pour mettre à jour le label du capteur
+void updateSensorLabel(lv_obj_t *label, const char* text) {
+    lv_label_set_text(label, text);
 }
 
 // Fonction pour faire bouger le servo de 0 à 180 degrés et vice-versa
@@ -72,6 +77,15 @@ void moveServo(PwmOut &servoPin, lv_obj_t *label) {
         ThisThread::sleep_for(10ms);
         updateServoLabel(label, pos);
     }
+}
+
+// Fonction pour bouger le servo à 90 degrés et revenir
+void moveServoTo90AndBack(PwmOut &servoPin, lv_obj_t *label) {
+    setServoAngle(servoPin, 90);
+    updateServoLabel(label, 90);
+    ThisThread::sleep_for(20ms);
+    setServoAngle(servoPin, 0);
+    updateServoLabel(label, 0);
 }
 
 // Fonction de rappel pour le premier bouton LVGL
@@ -119,7 +133,8 @@ int main() {
     
     // Initialiser LVGL
     threadLvgl.lock();
-    
+
+    // création des objet 2 boutons
     create_button1();
     create_button2();
 
@@ -134,27 +149,54 @@ int main() {
     lv_label_set_text_fmt(servo2_label, "Angle: %d deg", angle2);
 
     // Créer des labels pour afficher l'état des capteurs
-    lv_obj_t *sensor1_label = lv_label_create(lv_scr_act());
+    sensor1_label = lv_label_create(lv_scr_act());
     lv_obj_align(sensor1_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 210);
-    lv_label_set_text(sensor1_label, "Sensor 1: 1");
+    lv_label_set_text(sensor1_label, "Sensor 1: 0");
 
-    lv_obj_t *sensor2_label = lv_label_create(lv_scr_act());
+    sensor2_label = lv_label_create(lv_scr_act());
     lv_obj_align(sensor2_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 230);
-    lv_label_set_text(sensor2_label, "Sensor 2: 1");
+    lv_label_set_text(sensor2_label, "Sensor 2: 0");
 
-    lv_obj_t *sensor3_label = lv_label_create(lv_scr_act());
+    sensor3_label = lv_label_create(lv_scr_act());
     lv_obj_align(sensor3_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 250);
-    lv_label_set_text(sensor3_label, "Sensor 3: 1");
+    lv_label_set_text(sensor3_label, "Sensor 3: 0");
 
-    lv_obj_t *sensor4_label = lv_label_create(lv_scr_act());
+    sensor4_label = lv_label_create(lv_scr_act());
     lv_obj_align(sensor4_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 270);
-    lv_label_set_text(sensor4_label, "Sensor 4: 1");
+    lv_label_set_text(sensor4_label, "Sensor 4: 0");
+
+    // Créer un label pour afficher le message de détection de voiture
+    car_detected_label = lv_label_create(lv_scr_act());
+    lv_obj_align(car_detected_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 310);
+    lv_label_set_text(car_detected_label, "Voiture non détectée");
+    
+    // Créer un label pour afficher le message de détection de voiture
+    car_detected_label2 = lv_label_create(lv_scr_act());
+    lv_obj_align(car_detected_label2, LV_ALIGN_OUT_BOTTOM_MID, 0, 330);
+    lv_label_set_text(car_detected_label2, "Voiture non détectée");
 
     threadLvgl.unlock();
 
     while (1) {
-        // Vérifier l'état du capteur D6 et faire bouger le servo si le capteur est activé
-        if (sensor2.read() && sensor1.read() == 1) {
+        // Détection de voiture par le capteur 1
+        if (sensor1.read() == 0) {
+            updateSensorLabel(sensor1_label, "Sensor 1: 0");
+            updateSensorLabel(car_detected_label, "Voiture detectee");
+        } else {
+            updateSensorLabel(sensor1_label, "Sensor 1: 1");
+            updateSensorLabel(car_detected_label, "Voiture non detectee");
+        }
+
+        // Détection de voiture par le capteur 2
+        if (sensor2.read() == 0) {
+            updateSensorLabel(sensor2_label, "Sensor 2: 0");
+            updateSensorLabel(car_detected_label2, "Voiture detectee");
+        } else {
+            updateSensorLabel(sensor2_label, "Sensor 2: 1");
+            updateSensorLabel(car_detected_label2, "Voiture non detectee");
+        }
+        // Mouvement du servo 1 par le capteur 4
+        if (sensor4.read() == 0) {
             // Faire tourner le servomoteur de 0 à 180 degrés et vice-versa
             moveServo(servoPin1, servo1_label);
             // Revenir à la position d'origine
@@ -162,24 +204,39 @@ int main() {
             updateServoLabel(servo1_label, 0);
         }
 
-
-        if (sensor3.read() && sensor4.read() == 1) {
-            // Faire tourner le servomoteur de 0 à 180 degrés et vice-versa
+        // Mouvement du servo 2 par les capteurs 3 
+        if (sensor3.read() == 0 ) {
             moveServo(servoPin2, servo2_label);
             // Revenir à la position d'origine
             setServoAngle(servoPin2, 0);
             updateServoLabel(servo2_label, 0);
         }
+       
 
-        // Mettre à jour les labels des positions des servomoteurs
-        updateServoLabel(servo1_label, angle1);
-        updateServoLabel(servo2_label, angle2);
+        // Mettre à jour les labels des capteurs
+        if (sensor1.read() == 0) {
+            updateSensorLabel(sensor1_label, "Sensor 1: 0");
+        } else {
+            updateSensorLabel(sensor1_label, "Sensor 1: 1");
+        }
+        
+        if (sensor2.read() == 0) {
+            updateSensorLabel(sensor2_label, "Sensor 2: 0");
+        } else {
+            updateSensorLabel(sensor2_label, "Sensor 2: 1");
+        }
 
-        // Mettre à jour les labels des capteurs avec valeurs inversées
-        lv_label_set_text_fmt(sensor1_label, "Sensor 1: %d", !sensor1.read());
-        lv_label_set_text_fmt(sensor2_label, "Sensor 2: %d", !sensor2.read());
-        lv_label_set_text_fmt(sensor3_label, "Sensor 3: %d", !sensor3.read());
-        lv_label_set_text_fmt(sensor4_label, "Sensor 4: %d", !sensor4.read());
+        if (sensor3.read() == 0) {
+            updateSensorLabel(sensor3_label, "Sensor 3: 0");
+        } else {
+            updateSensorLabel(sensor3_label, "Sensor 3: 1");
+        }
+
+        if (sensor4.read() == 0) {
+            updateSensorLabel(sensor4_label, "Sensor 4: 0");
+        } else {
+            updateSensorLabel(sensor4_label, "Sensor 4: 1");
+        }
 
         // Boucle principale
         ThisThread::sleep_for(20ms);
